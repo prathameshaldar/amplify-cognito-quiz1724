1fb40e5a500d636d6a9365d7357b6cb113c76b2e
prathameshaldar <99965274+prathameshaldar@users.noreply.github.com>
2024-05-01 11:39:22 +0530

    Update quizData.js
    added 2nd question

--- src/quizData.js
+++ src/quizData.js
@@ -1,5 +1,10 @@
 const quizData = [
-   
+
+     {
+      question: "Which company developed the first commercial antivirus software?",
+      options: ["Symantec", "McAfee", "Norton", "Kaspersky Lab"],
+      answer: "McAfee"
+    },
   
     {
       question: "Which animal is featured in the official PHP logo?",
